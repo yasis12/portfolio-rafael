fd6dfd07de2fb5c4211818a05ada9fc144fce107
Rafael Yasis <rafaelyasis@gmail.com>
2023-12-23 11:43:37 -0600

    Created react app

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+//Component Imports
+import Portfolio from '../Portfolio/Portfolio';
+
+function App() {
+  return (
+    <>
+      <Portfolio />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import './Portfolio.css'
+
+function Portfolio() {
+  return (
+    <>
+      <h1>Hello Raf</h1>
+    </>
+  );
+}
+
+export default Portfolio;
