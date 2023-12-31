f855ad89da7b07334aeb30584a6032f4970aec7a
Rafael Yasis <rafaelyasis@gmail.com>
2023-12-31 11:31:57 -0600

    Added buttons

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,4 +1,12 @@
+//Styling Imports
 import './LandingPage.css'
+//MUI Imports
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AlarmIcon from '@mui/icons-material/Alarm';
+
 
 function LandingPage() {
 
@@ -16,47 +24,59 @@ function LandingPage() {
         <form onSubmit={submitButton}>
           <div className='button-row'>
             <div className='button-container'>
-              <button>[...]</button>
+                <Button variant="contained" color="secondary" size="large">Click Me</Button>
             </div>
             <div className='button-container'>
-              <button>[...]</button>
+            <Button variant="contained" color="error" size="large">Click Me</Button>
             </div>
             <div className='button-container'>
-              <button>[...]</button>
+            <Button variant="contained" color="primary" size="large">Click Me</Button>
             </div>
             <div className='button-container'>
-              <button>[...]</button>
+              <div id='radio-buttons'>
+                <input type="radio" /> 
+                <input type="radio" /> 
+                <input type="radio" /> 
+                <input type="radio" />
+              </div>
             </div>
             <div className='button-container'>
-              <button>[...]</button>
+            <IconButton color="secondary" aria-label="add an alarm">
+              <AlarmIcon />
+            </IconButton>
             </div>
             <div className='button-container'>
-              <button>[...]</button>
-            </div>
-          </div>
-          <div className='button-row'>
-            <div className='button-container'>
-              <button>[...]</button>
-            </div>
-            <div className='button-container'>
-              <button>[...]</button>
-            </div>
-            <div className='button-container'>
-              <button>[...]</button>
-            </div>
-            <div className='button-container'>
-              <button>[...]</button>
-            </div>
-            <div className='button-container'>
-              <button>[...]</button>
-            </div>
-            <div className='button-container'>
-              <button>[...]</button>
+            <IconButton aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
             </div>
           </div>
           <div className='button-row'>
             <div className='button-container'>
-              <button>[...]</button>
+              <input type="text" placeholder='Text' />
+            </div>
+            <div className='button-container'>
+              <input type="checkbox" />
+            </div>
+            <div className='button-container'>
+              <input type="date" />
+            </div>
+            <div className='button-container'>
+              <input type="file" />
+            </div>
+            <div className='button-container'>
+              <input type="password" placeholder='password'/> 
+            </div>
+            <div className='button-container'>
+              <input type="week" />
+            </div>
+          </div>
+          <div className='button-row'>
+            <div className='button-container'>
+              <input type="checkbox" />
+              <input type="checkbox" />
+              <input type="checkbox" />
+              <input type="checkbox" />
             </div>
             <div className='button-container'>
               <button>[...]</button>
